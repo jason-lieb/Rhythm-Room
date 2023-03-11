b3c7764c62c9dedb59aaaf9679b67c548180f046
MrTompkinsGamow <mhbrandenburgh@gmail.com>
2023-03-11 14:30:50 -0500

    part of the layout

--- client/src/components/pages/Profile.js
+++ client/src/components/pages/Profile.js
@@ -1,3 +1,14 @@
+import * as React from 'react'
+// import CssBaseline from '@mui/material/CssBaseline';
+// import Box from '@mui/material/Box';
+import Container from '@mui/material/Container'
+import Card from '@mui/material/Card'
+import CardActions from '@mui/material/CardActions'
+import CardContent from '@mui/material/CardContent'
+import Button from '@mui/material/Button'
+import Typography from '@mui/material/Typography'
+import Avatar from '@mui/material/Avatar'
+
 const AUTH_URL =
   'https://accounts.spotify.com/authorize?' +
   'client_id=538c7cde1253426896361ee2d3a79d9f' +
@@ -5,11 +16,63 @@ const AUTH_URL =
   '&redirect_uri=http://localhost:3000' +
   '&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state'
 
+const css = `
+  #main-container {
+    border-style:  solid;
+    border-width: medium;
+    border-color: black;
+    margin: 10px;
+    display: flex;
+    flex-direction: row;
+  }
+  #userinfo {
+    border-style:  solid;
+    border-width: medium;
+    border-color: red;
+    margin: 10px;
+  }
+  #playlistinfo {
+    border-style:  solid;
+    border-width: medium;
+    border-color: blue;
+    margin: 10px;
+  }
+`
+
 function Profile() {
   return (
-    <div>
-      <a href={AUTH_URL}>Login with Spotify</a>
-    </div>
+    <>
+      <style type="text/css">{css}</style>
+      <Container maxWidth="lg" id="main-container">
+        <Container id="userinfo">
+          <Card sx={{ minWidth: 275 }}>
+            <CardContent>
+              <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+              <Typography variant="h4" component="div">
+                Username
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <a href={AUTH_URL}>
+                <Button size="small">Connect your Spotify Account</Button>
+              </a>
+            </CardActions>
+            <CardContent>
+              <Typography variant="h5">About Me</Typography>
+              <Typography variant="p">
+                I was born in Ohio. I grew up on a farm and always listened to
+                Country music. Later in life I discovered Jazz. Please add me
+                and follow my recommendations!
+              </Typography>
+            </CardContent>
+          </Card>
+          {/* <div>
+            <a href={AUTH_URL}>Connect your Spotify account</a>
+          </div> */}
+        </Container>
+        <Container id="playlistinfo"></Container>
+      </Container>
+    </>
   )
 }
 
