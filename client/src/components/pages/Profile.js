903a562e7fb7608403b71fdbc46cafa3d9a78def
MrTompkinsGamow <mhbrandenburgh@gmail.com>
2023-03-11 15:06:47 -0500

    more content

--- client/src/components/pages/Profile.js
+++ client/src/components/pages/Profile.js
@@ -1,6 +1,6 @@
 import * as React from 'react'
 // import CssBaseline from '@mui/material/CssBaseline';
-// import Box from '@mui/material/Box';
+import Box from '@mui/material/Box'
 import Container from '@mui/material/Container'
 import Card from '@mui/material/Card'
 import CardActions from '@mui/material/CardActions'
@@ -8,6 +8,14 @@ import CardContent from '@mui/material/CardContent'
 import Button from '@mui/material/Button'
 import Typography from '@mui/material/Typography'
 import Avatar from '@mui/material/Avatar'
+import Stack from '@mui/material/Stack'
+import { useTheme } from '@mui/material/styles'
+import CardMedia from '@mui/material/CardMedia'
+import IconButton from '@mui/material/IconButton'
+import SkipPreviousIcon from '@mui/icons-material/SkipPrevious'
+import PlayArrowIcon from '@mui/icons-material/PlayArrow'
+import SkipNextIcon from '@mui/icons-material/SkipNext'
+import defaultPlaylistIcon from '../../assets/live-from-space.jpg'
 
 const AUTH_URL =
   'https://accounts.spotify.com/authorize?' +
@@ -31,7 +39,7 @@ const css = `
     border-color: red;
     margin: 10px;
   }
-  #playlistinfo {
+  #playlistsection {
     border-style:  solid;
     border-width: medium;
     border-color: blue;
@@ -40,6 +48,7 @@ const css = `
 `
 
 function Profile() {
+  const theme = useTheme()
   return (
     <>
       <style type="text/css">{css}</style>
@@ -65,12 +74,200 @@ function Profile() {
                 and follow my recommendations!
               </Typography>
             </CardContent>
+            <CardActions>
+              <a href="">
+                <Button size="small">Create/add a playlist</Button>
+              </a>
+            </CardActions>
           </Card>
           {/* <div>
             <a href={AUTH_URL}>Connect your Spotify account</a>
           </div> */}
         </Container>
-        <Container id="playlistinfo"></Container>
+        <Stack id="playlistsection">
+          <Container>
+            <Typography>Created Playlists</Typography>
+            {/* Individual Playlist Card */}
+            <Card sx={{ display: 'flex' }}>
+              <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+                <CardContent sx={{ flex: '1 0 auto' }}>
+                  <Typography component="div" variant="h5">
+                    Live From Space
+                  </Typography>
+                  <Typography
+                    variant="subtitle1"
+                    color="text.secondary"
+                    component="div"
+                  >
+                    Mac Miller
+                  </Typography>
+                </CardContent>
+                <Box
+                  sx={{ display: 'flex', alignItems: 'center', pl: 1, pb: 1 }}
+                >
+                  <IconButton aria-label="previous">
+                    {theme.direction === 'rtl' ? (
+                      <SkipNextIcon />
+                    ) : (
+                      <SkipPreviousIcon />
+                    )}
+                  </IconButton>
+                  <IconButton aria-label="play/pause">
+                    <PlayArrowIcon sx={{ height: 38, width: 38 }} />
+                  </IconButton>
+                  <IconButton aria-label="next">
+                    {theme.direction === 'rtl' ? (
+                      <SkipPreviousIcon />
+                    ) : (
+                      <SkipNextIcon />
+                    )}
+                  </IconButton>
+                </Box>
+              </Box>
+              <CardMedia
+                component="img"
+                sx={{ width: 151 }}
+                image={defaultPlaylistIcon}
+                alt="Live from space album cover"
+              />
+            </Card>
+            {/* Individual Playlist Card */}
+            <Card sx={{ display: 'flex' }}>
+              <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+                <CardContent sx={{ flex: '1 0 auto' }}>
+                  <Typography component="div" variant="h5">
+                    Greatest Hits
+                  </Typography>
+                  <Typography
+                    variant="subtitle1"
+                    color="text.secondary"
+                    component="div"
+                  >
+                    The Beatles
+                  </Typography>
+                </CardContent>
+                <Box
+                  sx={{ display: 'flex', alignItems: 'center', pl: 1, pb: 1 }}
+                >
+                  <IconButton aria-label="previous">
+                    {theme.direction === 'rtl' ? (
+                      <SkipNextIcon />
+                    ) : (
+                      <SkipPreviousIcon />
+                    )}
+                  </IconButton>
+                  <IconButton aria-label="play/pause">
+                    <PlayArrowIcon sx={{ height: 38, width: 38 }} />
+                  </IconButton>
+                  <IconButton aria-label="next">
+                    {theme.direction === 'rtl' ? (
+                      <SkipPreviousIcon />
+                    ) : (
+                      <SkipNextIcon />
+                    )}
+                  </IconButton>
+                </Box>
+              </Box>
+              <CardMedia
+                component="img"
+                sx={{ width: 151 }}
+                image={defaultPlaylistIcon}
+                alt="Live from space album cover"
+              />
+            </Card>
+          </Container>
+          <Container>
+            <Typography>Liked Playlists</Typography>
+            {/* Individual Playlist Card */}
+            <Card sx={{ display: 'flex' }}>
+              <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+                <CardContent sx={{ flex: '1 0 auto' }}>
+                  <Typography component="div" variant="h5">
+                    Are You Experienced?
+                  </Typography>
+                  <Typography
+                    variant="subtitle1"
+                    color="text.secondary"
+                    component="div"
+                  >
+                    Jimi Hendrix
+                  </Typography>
+                </CardContent>
+                <Box
+                  sx={{ display: 'flex', alignItems: 'center', pl: 1, pb: 1 }}
+                >
+                  <IconButton aria-label="previous">
+                    {theme.direction === 'rtl' ? (
+                      <SkipNextIcon />
+                    ) : (
+                      <SkipPreviousIcon />
+                    )}
+                  </IconButton>
+                  <IconButton aria-label="play/pause">
+                    <PlayArrowIcon sx={{ height: 38, width: 38 }} />
+                  </IconButton>
+                  <IconButton aria-label="next">
+                    {theme.direction === 'rtl' ? (
+                      <SkipPreviousIcon />
+                    ) : (
+                      <SkipNextIcon />
+                    )}
+                  </IconButton>
+                </Box>
+              </Box>
+              <CardMedia
+                component="img"
+                sx={{ width: 151 }}
+                image={defaultPlaylistIcon}
+                alt="Live from space album cover"
+              />
+            </Card>
+            {/* Individual Playlist Card */}
+            <Card sx={{ display: 'flex' }}>
+              <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+                <CardContent sx={{ flex: '1 0 auto' }}>
+                  <Typography component="div" variant="h5">
+                    Dinner Party
+                  </Typography>
+                  <Typography
+                    variant="subtitle1"
+                    color="text.secondary"
+                    component="div"
+                  >
+                    Various Artists
+                  </Typography>
+                </CardContent>
+                <Box
+                  sx={{ display: 'flex', alignItems: 'center', pl: 1, pb: 1 }}
+                >
+                  <IconButton aria-label="previous">
+                    {theme.direction === 'rtl' ? (
+                      <SkipNextIcon />
+                    ) : (
+                      <SkipPreviousIcon />
+                    )}
+                  </IconButton>
+                  <IconButton aria-label="play/pause">
+                    <PlayArrowIcon sx={{ height: 38, width: 38 }} />
+                  </IconButton>
+                  <IconButton aria-label="next">
+                    {theme.direction === 'rtl' ? (
+                      <SkipPreviousIcon />
+                    ) : (
+                      <SkipNextIcon />
+                    )}
+                  </IconButton>
+                </Box>
+              </Box>
+              <CardMedia
+                component="img"
+                sx={{ width: 151 }}
+                image={defaultPlaylistIcon}
+                alt="Live from space album cover"
+              />
+            </Card>
+          </Container>
+        </Stack>
       </Container>
     </>
   )
