afeb7f258d407c7e367412bda3b65c0ea679a4f5
MrTompkinsGamow <mhbrandenburgh@gmail.com>
2023-03-12 23:00:22 -0400

    some profile changes

--- client/src/components/pages/Profile.jsx
+++ client/src/components/pages/Profile.jsx
@@ -1,20 +1,21 @@
-import * as React from 'react';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import ConcertImg from '../../assets/music.jpg';
-import Box from '@mui/material/Box';
-import IconButton from '@mui/material/IconButton';
-import SkipPreviousIcon from '@mui/icons-material/SkipPrevious';
-import PlayArrowIcon from '@mui/icons-material/PlayArrow';
-import SkipNextIcon from '@mui/icons-material/SkipNext';
-import Avatar from '@mui/material/Avatar';
-import { useTheme } from '@mui/material/styles';
-
-
+import * as React from 'react'
+import Card from '@mui/material/Card'
+import CardActions from '@mui/material/CardActions'
+import CardContent from '@mui/material/CardContent'
+import CardMedia from '@mui/material/CardMedia'
+import Button from '@mui/material/Button'
+import Typography from '@mui/material/Typography'
+import ConcertImg from '../../assets/music.jpg'
+import Box from '@mui/material/Box'
+import IconButton from '@mui/material/IconButton'
+import SkipPreviousIcon from '@mui/icons-material/SkipPrevious'
+import PlayArrowIcon from '@mui/icons-material/PlayArrow'
+import SkipNextIcon from '@mui/icons-material/SkipNext'
+import Avatar from '@mui/material/Avatar'
+import { useTheme } from '@mui/material/styles'
+import { useQuery } from '@apollo/client'
+import { QUERY_USER } from '../utils/queries'
+import { useParams } from 'react-router-dom'
 
 const css = `
   .container-box {
@@ -74,69 +75,107 @@ const css = `
 `
 
 export default function Login() {
-  const theme = useTheme();
+  const theme = useTheme()
+  const { profileId } = useParams()
+  const { loading, data } = useQuery(QUERY_USER, {
+    variables: { userId: profileId },
+  })
+
+  if (loading) {
+    return <div>Loading...</div>
+  }
+
   return (
-    <div className='container-box'>
-      <style type = "text/css">{css}</style>
-      <Card className = "card">
-        <div className='left-content'>
-          <div className='name-header'>
+    <div className="container-box">
+      {console.log(data)}
+      <style type="text/css">{css}</style>
+      <Card className="card">
+        <div className="left-content">
+          <div className="name-header">
             <Avatar
               alt="Remy Sharp"
               src="/static/images/avatar/1.jpg"
               sx={{ width: 100, height: 100 }}
             />
-            <Typography className='user-name'>
-              Name Example
-            </Typography>
+            <Typography className="user-name">Name Example</Typography>
           </div>
           <CardContent>
-            <Typography className='text' gutterBottom variant="h5" component="div">
+            <Typography
+              className="text"
+              gutterBottom
+              variant="h5"
+              component="div"
+            >
               About Me:
             </Typography>
-            <Typography className='text' variant="body2" color="text.secondary">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus expedita facilis consequuntur rerum saepe tenetur eum enim quaerat, eaque assumenda cupiditate fugiat sunt fuga hic vero eligendi suscipit sit qui voluptate magnam. Quos reiciendis praesentium culpa nostrum nulla earum, enim libero aperiam at inventore? Iure quis voluptatem quas deserunt maiores. Cupiditate fugit quod eos magni. Ipsam ad possimus voluptatibus dicta culpa accusantium quam! Repellat ad veniam et alias earum nisi consectetur tempore excepturi sunt, sint, a atque vel blanditiis aspernatur iusto amet ut assumenda? Iure corporis quis sed facilis placeat ab repellat, incidunt vitae esse eius saepe tempore, vel consequuntur?
+            <Typography className="text" variant="body2" color="text.secondary">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus
+              expedita facilis consequuntur rerum saepe tenetur eum enim
+              quaerat, eaque assumenda cupiditate fugiat sunt fuga hic vero
+              eligendi suscipit sit qui voluptate magnam. Quos reiciendis
+              praesentium culpa nostrum nulla earum, enim libero aperiam at
+              inventore? Iure quis voluptatem quas deserunt maiores. Cupiditate
+              fugit quod eos magni. Ipsam ad possimus voluptatibus dicta culpa
+              accusantium quam! Repellat ad veniam et alias earum nisi
+              consectetur tempore excepturi sunt, sint, a atque vel blanditiis
+              aspernatur iusto amet ut assumenda? Iure corporis quis sed facilis
+              placeat ab repellat, incidunt vitae esse eius saepe tempore, vel
+              consequuntur?
             </Typography>
           </CardContent>
         </div>
-        <div className='right-content'>
-            <Card className='liked-playlist'>
-              <Typography>Liked Playlist</Typography>
-              <Card sx={{ display: 'flex' }}>
-                <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-                  <CardContent sx={{ flex: '1 0 auto' }}>
-                    <Typography component="div" variant="h5">
-                      Live From Space
-                    </Typography>
-                    <Typography variant="subtitle1" color="text.secondary" component="div">
-                      Mac Miller
-                    </Typography>
-                  </CardContent>
-                  <Box sx={{ display: 'flex', alignItems: 'center', pl: 1, pb: 1 }}>
-                    <IconButton aria-label="previous">
-                      {theme.direction === 'rtl' ? <SkipNextIcon /> : <SkipPreviousIcon />}
-                    </IconButton>
-                    <IconButton aria-label="play/pause">
-                      <PlayArrowIcon sx={{ height: 38, width: 38 }} />
-                    </IconButton>
-                    <IconButton aria-label="next">
-                      {theme.direction === 'rtl' ? <SkipPreviousIcon /> : <SkipNextIcon />}
-                    </IconButton>
-                  </Box>
+        <div className="right-content">
+          <Card className="liked-playlist">
+            <Typography>Liked Playlist</Typography>
+            <Card sx={{ display: 'flex' }}>
+              <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+                <CardContent sx={{ flex: '1 0 auto' }}>
+                  <Typography component="div" variant="h5">
+                    Live From Space
+                  </Typography>
+                  <Typography
+                    variant="subtitle1"
+                    color="text.secondary"
+                    component="div"
+                  >
+                    Mac Miller
+                  </Typography>
+                </CardContent>
+                <Box
+                  sx={{ display: 'flex', alignItems: 'center', pl: 1, pb: 1 }}
+                >
+                  <IconButton aria-label="previous">
+                    {theme.direction === 'rtl' ? (
+                      <SkipNextIcon />
+                    ) : (
+                      <SkipPreviousIcon />
+                    )}
+                  </IconButton>
+                  <IconButton aria-label="play/pause">
+                    <PlayArrowIcon sx={{ height: 38, width: 38 }} />
+                  </IconButton>
+                  <IconButton aria-label="next">
+                    {theme.direction === 'rtl' ? (
+                      <SkipPreviousIcon />
+                    ) : (
+                      <SkipNextIcon />
+                    )}
+                  </IconButton>
                 </Box>
-                <CardMedia
-                  component="img"
-                  sx={{ width: 151 }}
-                  image="/static/images/cards/live-from-space.jpg"
-                  alt="Live from space album cover"
-                />
-                </Card>
-              </Card>
-              <Card className='created-playlist'>
-                <Typography>Created Playlist</Typography>
-              </Card>
+              </Box>
+              <CardMedia
+                component="img"
+                sx={{ width: 151 }}
+                image="/static/images/cards/live-from-space.jpg"
+                alt="Live from space album cover"
+              />
+            </Card>
+          </Card>
+          <Card className="created-playlist">
+            <Typography>Created Playlist</Typography>
+          </Card>
         </div>
       </Card>
     </div>
-  );
+  )
 }
