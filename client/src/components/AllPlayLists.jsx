89892af9b90fc4cdacf0365128c3cdb072219ed7
waustin45 <108015666+waustin45@users.noreply.github.com>
2023-03-14 12:51:03 -0400

    topull

--- client/src/components/AllPlayLists.jsx
+++ client/src/components/AllPlayLists.jsx
@@ -1,11 +1,16 @@
 import ImageList from '@mui/material/ImageList'
 import ImageListItem from '@mui/material/ImageListItem'
+
 import { useQuery } from '@apollo/client'
 import { Link } from 'react-router-dom'
 import { QUERY_ALL_PLAYLISTS } from '../utils/queries'
+
 import likeBtn from './likeBtn'
 import Box from '@mui/material/Box'
 import ThumbUpIcon from '@mui/icons-material/ThumbUp';
+
+
+
 export default function AllPlayLists() {
   // fetch the information needed for the playlist cards and push it as props into the PlaylistCard component
   const { loading, data } = useQuery(QUERY_ALL_PLAYLISTS)
@@ -16,8 +21,10 @@ export default function AllPlayLists() {
       {!loading ? (
         playlists.map((info) => {
           return (
-            <Link style={{ zIndex: 0 }} key={info._id} to={`/Playlist/${info._id}`}>
-              <ImageListItem>
+            <Link style={{ zIndex: 0, position:'relative' }} key={info._id} to={`/Playlist/${info._id}`}>
+              <ImageListItem
+              style={{ minHeight: 10, zIndex: -2, position:'relative' }}
+              >
                 <img
                   src={`${info.images[0].url}?w=248&fit=crop&auto=format`}
                   srcSet={`${info.images[0].url}?w=248&fit=crop&auto=format&dpr=2 2x`}
@@ -25,8 +32,8 @@ export default function AllPlayLists() {
                   loading="lazy"
                   style={{ minHeight: 10 }}
                 />
-                <div className='like-btn'>
-                  <ThumbUpIcon fontSize='large'/>
+                <div className='like-btn-home'>
+                  <ThumbUpIcon fontSize='large' sx={{ zIndex: 1 }}/>
                 </div>
               </ImageListItem>
             </Link>
