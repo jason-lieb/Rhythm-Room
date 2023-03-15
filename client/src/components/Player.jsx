b061b315956e4a59972244160b6f8cf025fe35dc
Lantao home PC <lantaook@gmail.com>
2023-03-14 20:33:22 -0400

    player fix

--- client/src/components/Player.jsx
+++ client/src/components/Player.jsx
@@ -2,20 +2,22 @@ import React, { useEffect, useState } from 'react'
 import SpotifyPlayer from 'react-spotify-web-playback'
 import { useSpotifyApi } from '../utils/SpotifyApiContext'
 
-export default function Player(songUri) {
+export default function Player({ songUri }) {
   const [spotifyApi] = useSpotifyApi()
-  useEffect(() => setPlay(true), [songUri])
   const [play, setPlay] = useState(false)
+  useEffect(() => {
+    setPlay(true)
+  }, [songUri])
 
   if (!spotifyApi.getAccessToken()) return null
   return (
     <SpotifyPlayer
       token={spotifyApi.getAccessToken()}
+      play={play}
       callback={(state) => {
         if (!state.isPlaying) setPlay(false)
       }}
-      play={play}
-      uris={['spotify:track:3jHBgKdLCf46aP3HRI0WYv']}
+      uris={songUri ? [songUri] : []}
     />
   )
 }
