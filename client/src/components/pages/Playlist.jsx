b1149902b0e0b93671604d018fd88c0365bb5f5d
waustin45 <108015666+waustin45@users.noreply.github.com>
2023-03-15 15:09:55 -0400

    to pull

--- client/src/components/pages/Playlist.jsx
+++ client/src/components/pages/Playlist.jsx
@@ -107,11 +107,11 @@ export default function Playlist() {
   return (
     <div className="playlistContainer">
       <style type="text/css">{css}</style>
-      <Container>
+      <Container maxWidth="lg">
         <div className="like-btn" onClick={likePlaylist}>
           <ThumbUpIcon fontSize="large" cursor="pointer" />
         </div>
-        <Grid container spacing={2} className="header">
+        <Grid container spacing={1} className="header">
           <Grid item xs={12} className="imgContainer">
             <img
               className="img"
@@ -163,6 +163,7 @@ export default function Playlist() {
             <Typography variant="subtitle2">Length</Typography>
           </Grid>
         </Grid>
+        <div style={{ marginTop: 10}}>
         {playlist.items &&
           playlist.items.map((song, index) => (
             <Song
@@ -175,6 +176,7 @@ export default function Playlist() {
               chooseTrack={chooseTrack}
             />
           ))}
+          </div>
       </Container>
       <Container sx={{ mb: 3 }}>
         <Grid container spacing={2}>
