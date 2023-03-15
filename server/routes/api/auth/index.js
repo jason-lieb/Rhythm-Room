d34e2de8474532584f1310a5607280edef021648
Jason Lieb <Jason.lieb@outlook.com>
2023-03-15 15:31:12 -0400

    add console

--- server/routes/api/auth/index.js
+++ server/routes/api/auth/index.js
@@ -40,6 +40,7 @@ router.post('/login', async (req, res) => {
         expiresIn: body.expires_in,
       })
     } else {
+      console.log('------------------- ERROR -------------------')
       console.log(body)
       console.error(err)
       res.sendStatus(500).json({ body, err })
