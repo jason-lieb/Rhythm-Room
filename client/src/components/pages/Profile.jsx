8d698940bd49e03772d18bedd403a2a319bbbc72
MrTompkinsGamow <mhbrandenburgh@gmail.com>
2023-03-15 11:18:46 -0400

    about me toggle edit feature

--- client/src/components/pages/Profile.jsx
+++ client/src/components/pages/Profile.jsx
@@ -112,7 +112,7 @@ export default function Profile() {
   }
 
   //functions for About Me section
-
+  const [openEditBox, setOpenEditBox] = useState(false)
   const handleAboutTextChange = (event) => {
     const { value } = event.target
     setAboutText(value)
@@ -123,6 +123,33 @@ export default function Profile() {
       variables: { about: aboutText, id: profileId },
     })
     setAboutTextDisplay(data.addAbout.about)
+    setOpenEditBox(false)
+  }
+
+  const editAboutMe = () => {
+    if (!openEditBox) {
+      return (
+        <Button variant="text" onClick={() => setOpenEditBox(true)}>
+          Edit
+        </Button>
+      )
+    } else if (openEditBox) {
+      return (
+        <>
+          <TextField
+            id="outlined-multiline-static"
+            label="Multiline"
+            multiline
+            rows={4}
+            defaultValue="Default Value"
+            onChange={handleAboutTextChange}
+          ></TextField>
+          <Button variant="contained" onClick={submitAbout}>
+            Submit
+          </Button>
+        </>
+      )
+    }
   }
 
   if (loading) {
@@ -162,17 +189,7 @@ export default function Profile() {
             <Typography className="text" variant="body2" color="text.secondary">
               {aboutTextDisplay}
             </Typography>
-            <TextField
-              id="outlined-multiline-static"
-              label="Multiline"
-              multiline
-              rows={4}
-              defaultValue="Default Value"
-              onChange={handleAboutTextChange}
-            ></TextField>
-            <Button variant="contained" onClick={submitAbout}>
-              Submit
-            </Button>
+            {editAboutMe()}
           </CardContent>
         </div>
 
